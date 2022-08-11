868f5f2921a9dbf7a92f891567d0747eb79f8b92
Hung Vo <hung.vo@nfq.asia>
2022-08-11 22:26:22 +0700

    remove redundant

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -61,7 +61,7 @@ const dataPie = [
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
 
 const RADIAN = Math.PI / 180
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5
   const x = cx + radius * Math.cos(-midAngle * RADIAN)
   const y = cy + radius * Math.sin(-midAngle * RADIAN)
